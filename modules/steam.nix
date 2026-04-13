d33b34ed08848c6d87d660af04648dd5422ee0f7
Dominic Grimaldi <dev@grimoutlook.dev>
2026-04-12 20:52:31 -0500

    Commented everything out to fix installs.
    Will enable piece by piece to see what is breaking stuff

--- modules/steam.nix
+++ modules/steam.nix
@@ -1,17 +1,17 @@
 {
-  programs = {
-    # Enable gamemode for game optimizations
-    gamemode.enable = true;
-
-    steam = {
-      enable = true;
-
-      # Whether to enable Load the extest library into Steam, to translate X11
-      # input events to uinput events (e.g. for using Steam Input on Wayland) .
-      extest.enable = true;
-
-      # Open ports in the firewall for Steam Local Network Game Transfers.
-      localNetworkGameTransfers.openFirewall = true;
-    };
-  };
+  # programs = {
+  #   # Enable gamemode for game optimizations
+  #   gamemode.enable = true;
+  #
+  #   steam = {
+  #     enable = true;
+  #
+  #     # Whether to enable Load the extest library into Steam, to translate X11
+  #     # input events to uinput events (e.g. for using Steam Input on Wayland) .
+  #     extest.enable = true;
+  #
+  #     # Open ports in the firewall for Steam Local Network Game Transfers.
+  #     localNetworkGameTransfers.openFirewall = true;
+  #   };
+  # };
 }
